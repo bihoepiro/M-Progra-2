3b4e15fbaeb868f621eeed874b1b727cd7b5c5f8
bihoepiro <83984593+bihoepiro@users.noreply.github.com>
2022-04-05 20:34:17 -0500

    Add files via upload

---
+++ S1/Ejemplo 1/funciones.cpp
@@ -0,0 +1,10 @@
+//
+// Created by HP on 4/04/2022.
+//
+
+#include "funciones.h"
+#include <iostream>
+
+void sumar(int &n, int i) {
+    n=10+i;
+}
---
+++ S1/Ejemplo 1/funciones.h
@@ -0,0 +1,11 @@
+//
+// Created by HP on 4/04/2022.
+//
+
+#ifndef EJEMPLO_1_FUNCIONES_H
+#define EJEMPLO_1_FUNCIONES_H
+using namespace std;
+
+void sumar(int &n, int i);
+
+#endif //EJEMPLO_1_FUNCIONES_H
