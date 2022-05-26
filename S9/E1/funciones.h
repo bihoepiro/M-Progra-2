82feeefc7d7a321680c1f3325ed75df26f085040
bihoepiro <83984593+bihoepiro@users.noreply.github.com>
2022-05-26 13:25:56 -0500

    Add files via upload

---
+++ S9/E1/funciones.h
@@ -0,0 +1,12 @@
+//
+// Created by Bihonda Epiquien  on 24/05/22.
+//
+
+#ifndef E1_FUNCIONES_H
+#define E1_FUNCIONES_H
+
+int PedirDatos();
+void CrearMatriz(int **&pM, int dimension);
+void MatrizSuma(int **&pM, int **matriz1, int **matriz2, int dimension);
+void ImprimirMatriz(int **matriz, int dimension);
+#endif //E1_FUNCIONES_H
